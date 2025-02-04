3b1a6d44620707a44eb22d5c839436080f5f88bb
William Pettersson <wipe2400@student.miun.se>
2025-02-04 17:24:28 +0100

    Add function to get JSON data on load

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// Declare variables
+let scheduleData;
+const tableBody = document.getElementById("table");
+
+// Declare functions
+async function getJSON(){
+    try {
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+        if (!response.ok){
+            throw new Error("Invalid response!");
+        }
+        scheduleData = await response.json();
+    } catch (error){
+        console.error(error);
+        tableBody.innerHTML = "<tr><td>Ett</td><td>Problem</td><td>Uppstod!</td></tr>";
+    }
+}
+
+// Event handlers
+addEventListener("load", getJSON);
