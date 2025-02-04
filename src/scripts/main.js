f9970c764e7ec7498b24b89f89acc7647432dfee
William Pettersson <wipe2400@student.miun.se>
2025-02-04 17:38:55 +0100

    Add function to fill HTML table with data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,10 +12,24 @@ async function getJSON(){
             throw new Error("Invalid response!");
         }
         scheduleData = await response.json();
+        populateTable(scheduleData);
     } catch (error){
         console.error(error);
         tableBody.innerHTML = "<tr><td>Ett</td><td>Problem</td><td>Uppstod!</td></tr>";
     }
+}
+
+function populateTable(data){
+    let tableHTML = "";
+    data.forEach(course => {
+        tableHTML +=
+        `<tr>
+            <td><a target="_blank" href="${course.syllabus}">${course.code}</a></td>
+            <td>${course.coursename}</td>
+            <td>${course.progression}</td>
+        </tr>`;
+    });
+    tableBody.innerHTML = tableHTML;
 }
 
 // Event handlers
